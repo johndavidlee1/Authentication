562b9d518b09da3688d148852fcd94f2a3ecce10
johndavidlee1 <54914773+johndavidlee1@users.noreply.github.com>
2019-09-04 15:34:37 -0600

    Set up firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyDh3em6EBOzru9Hciuv5b4_2EJjiSzV8Q0",
+  authDomain: "authentication-be8eb.firebaseapp.com",
+  databaseURL: "https://authentication-be8eb.firebaseio.com",
+  projectId: "authentication-be8eb",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "455777256485",
+  appId: "1:455777256485:web:d7b1d57447f8d7f58eebcc"
+};
 };
 
 firebase.initializeApp(config);
